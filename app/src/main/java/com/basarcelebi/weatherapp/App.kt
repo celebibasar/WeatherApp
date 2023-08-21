e055bc0b7e14e3b1de3c4e831d2a5c49c9e91899
Başar Çelebi <75533431+celebibasar@users.noreply.github.com>
2023-08-21 16:26:24 +0300

    API Added

---
+++ app/src/main/java/com/basarcelebi/weatherapp/App.kt
@@ -0,0 +1,24 @@
+package com.basarcelebi.weatherapp
+
+import android.app.Application
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(module {
+                single {
+                    Retrofit
+                        .Builder()
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .baseUrl("http://dataservice.accuweather.com/")
+                        .build()
+                }
+            })
+        }
+    }
+}
